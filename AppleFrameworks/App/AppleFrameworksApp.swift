01662c683c1ed85cc41abe27ce31a63cb3b00cc6
Dima <dmitriy.mkrtumyan@gmail.com>
2024-02-14 23:46:01 +0400

    Init

---
+++ AppleFrameworks/App/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Dmitriy Mkrtumyan on 14.02.24.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainGridView()
+        }
+    }
+}
---
+++ AppleFrameworks/MainPage/View/MainGridView.swift
@@ -0,0 +1,18 @@
+//
+//  MainGridView.swift
+//  AppleFrameworks
+//
+//  Created by Dmitriy Mkrtumyan on 14.02.24.
+//
+
+import SwiftUI
+
+struct MainGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainGridView()
+}
