001a5b706084b2ad56a0327a3b38328ef7c1ea5f
Dima <dmitriy.mkrtumyan@gmail.com>
2024-02-16 14:05:25 +0400

    Add SafariView

---
+++ AppleFrameworks/MainPage/ViewModel/FrameworkGridViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FrameworkGridViewModel.swift
+//  AppleFrameworks
+//
+//  Created by Dmitriy Mkrtumyan on 15.02.24.
+//
+
+import Foundation
---
+++ AppleFrameworks/SafariView/SafariView.swift
@@ -0,0 +1,8 @@
+//
+//  SafariView.swift
+//  AppleFrameworks
+//
+//  Created by Dmitriy Mkrtumyan on 16.02.24.
+//
+
+import Foundation
